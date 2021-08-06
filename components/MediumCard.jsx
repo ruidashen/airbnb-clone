61f86f1bf8f98fa9be5cd9157cd033cd3663ac4d
ruida-shen <rshen@fanrally.com>
2021-08-06 01:23:48 -0700

    Initial commit

---
+++ components/MediumCard.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import React from "react";
+
+export default function MediumCard({ img, title }) {
+  console.log(title);
+  return (
+    <div className="cursor-pointer hover:scale-105 transition duration-300 ease-out">
+      <div className="relative h-44 w-44 sm:w-80 sm:h-80">
+        <Image src={img} layout="fill" className="rounded-xl" />
+      </div>
+      <h3 className="text-2xl mt-3">{title}</h3>
+    </div>
+  );
+}
---
+++ components/SmallCard.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import React from "react";
+
+export default function SmallCard({ img, location, distance }) {
+  return (
+    <div className="flex items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer hover:bg-gray-100 hover:scale-110 transition duration-200 ease-out">
+      <div className="relative h-16 w-16">
+        <Image src={img} layout="fill" className="rounded-lg" />
+      </div>
+
+      <div>
+        <h2>{location}</h2>
+        <h3>{distance}</h3>
+      </div>
+    </div>
+  );
+}
